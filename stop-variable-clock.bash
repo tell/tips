79fdc425fce18c829c79624c6eae763ade84bc59
Tadanori TERUYA <tadanori.teruya@gmail.com>
2015-05-31 12:10:47 +0900

    update

--- stop-variable-clock.bash
+++ stop-variable-clock.bash
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-! { expr "$1" : '[0-9]*' > /dev/null; } && echo ERROR: Not a number. && exit 1
+show_clocks() {
+	echo Available freqs.:
+	cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies
+	echo Available governors:
+	cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors
+	return 0
+}
+
+! { expr "$1" : '[0-9]*' > /dev/null; } && echo ERROR: Not a number. && show_clocks && exit 1
 
 freq=$1
 echo The frequencies of all CPUs are fixed as: $freq KHz.
