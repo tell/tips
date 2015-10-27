112c318cf8c035459beff1e1c3109e3ee6d29fc5
Tadanori TERUYA <tadanori.teruya@gmail.com>
2015-10-27 14:03:24 +0900

    update

--- manipulate-cpu/stop-variable-freq.bash
+++ manipulate-cpu/stop-variable-freq.bash
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+function usage() {
+	echo "Usage: $0 frequency"
+}
+
 function show_clocks() {
-	echo Available freqs.:
+	echo Available frequencies.:
 	cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies
 	echo Available governors:
 	cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors
 	return 0
 }
 
-! { expr "$1" : '[0-9]*' > /dev/null; } && echo ERROR: "$1": Not a number. && show_clocks && exit 1
+! { expr "$1" : '[0-9]*' > /dev/null; } && echo ERROR: "$1": Not a number. && usage && show_clocks && exit 1
 
 freq=$1
 echo The frequencies of all CPUs are fixed as: $freq KHz.
