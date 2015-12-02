a1dab302ad4884b1099d4024b9e549c217be105a
Tadanori TERUYA <tadanori.teruya@gmail.com>
2015-12-02 22:14:12 +0900

    update build script

--- toolchain/boost-linux.bash
+++ toolchain/boost-linux.bash
@@ -2,25 +2,38 @@
 
 set -e
 
+dont='' # '-n'
+
 function compile() {
+    set -x
     toolset=$1
-    version=$2
-    prefix="$3"
+    toolset_name=$2
+    boost_version=$3
+    prefix="$4"
 
-    ! [ -d "$prefix" ] && echo "$prefix : Not found" && exit 1
+    ! [ -d "$prefix" ] && echo "error $prefix : Not found" && exit 1
 
-    gittag="boost-$version"
-    git checkout -b $gittag $gittag && :
+    gittag="boost-$boost_version"
+    git checkout -fb local/$gittag $gittag && :
     git submodule update
     git submodule foreach 'git checkout . && git clean -fd'
 
+    common_options="toolset=$toolset threading=multi address-model=64 link=static,shared"
+    common_path="$boost_version/$toolset_name"
+    set +x
+
     ./b2 headers
-    temp="$version/$toolset/debug"
-    ./b2 -j 3 install --stagedir="stage/$temp" --prefix="$prefix/$temp" \
-        toolset=$toolset threading=multi variant=debug address-model=64 link=static,shared
-    temp="$version/$toolset/release"
-    ./b2 -j 3 install --stagedir="stage/$temp" --prefix="$prefix/$temp" \
-        toolset=$toolset threading=multi variant=release address-model=64 link=static,shared
+    variants=('debug' 'release')
+    for v in ${variants[@]}; do
+        cpath_full="$common_path/$v"
+        stagedir="stage/$cpath_full"
+        prefixdir="$prefix/$cpath_full"
+        ./b2 $dont -j 3 install \
+            --stagedir="$stagedir" \
+            --prefix="$prefixdir" \
+            variant=$v \
+            $common_options
+    done
 }
 
-compile gcc 1.59.0 "/opt/local/boost"
+compile gcc gcc-4.8 1.59.0 "/opt/local/boost"
